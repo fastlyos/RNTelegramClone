c3439db745f1a277a67630158f6ba175e69a5da6
Khoa Le <lnanhkhoa303@gmail.com>
2020-11-03 23:36:50 +0700

    init prepare for scale project

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    eqeqeq: [2, 'smart'],
+    'no-unused-vars': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'arrow-body-style': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-use-before-define': 'off',
+    'no-underscore-dangle': 'off',
+    'react/jsx-no-bind': 'off',
+    'global-require': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'consistent-return': 'off',
+    'max-len': [2, 300, 4],
+  },
+  globals: {
+    arguments: true,
+    strapi: true,
+    __DEV__: true,
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+};
