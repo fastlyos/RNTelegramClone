ed2c1dddd16cf92c1cfda75e9e4e170290a95c09
Khoa Le <khoa.le@gigacover.com>
2021-01-14 03:04:00 +0700

    final setup

---
+++ components/text/Text.stories.js
@@ -0,0 +1,11 @@
+//
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { text } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react-native";
+import CenterView from "@app/components/center-view/center-view";
+import Text from "./text";
+
+storiesOf("Text", module)
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add("with text", () => <Text type={"h1"}>{"Hello Button"}</Text>);
